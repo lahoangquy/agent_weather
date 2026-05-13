e09c304ef54e6c0de667cff4badad2ba3d468482
lahoangquy <62043234+lahoangquy@users.noreply.github.com>
2026-05-13 21:39:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,10 @@ app.secret_key = 'your_secret_key'
 @app.route('/')
 def home():
     session['thread_id'] = str(uuid.uuid4())
+
     if 'messages' not in session:
         session['messages'] = []
-    print('home', session)
+
     return render_template('chat.html', messages=session['messages'])
 
 @app.route('/send', methods=['POST'])
@@ -18,17 +19,42 @@ def send():
     user_message = request.form['message']
     user_lat = request.form.get('latitude')
     user_lon = request.form.get('longitude')
-    print(user_lat, user_lon)
 
     if user_lat and user_lon:
-        session['user_location'] = {'lat': user_lat, 'lon': user_lon}
+        session['user_location'] = {
+            'lat': user_lat,
+            'lon': user_lon
+        }
 
-    response = agent.invoke({"messages": [{'role': 'user', 'content':user_message}]},
-                                 {"configurable": {"thread_id":session['thread_id']}})
-    session['messages'].append({'type': 'human', 'content': user_message})
-    session['messages'].append({'type': 'ai', 'content': response['messages'][-1].content})
+    response = agent.invoke(
+        {
+            "messages": [
+                {
+                    'role': 'user',
+                    'content': user_message
+                }
+            ]
+        },
+        {
+            "configurable": {
+                "thread_id": session['thread_id']
+            }
+        }
+    )
+
+    session['messages'].append({
+        'type': 'human',
+        'content': user_message
+    })
+
+    session['messages'].append({
+        'type': 'ai',
+        'content': response['messages'][-1].content
+    })
+
     session.modified = True
-    print(session)
+
     return redirect(url_for('home'))
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
