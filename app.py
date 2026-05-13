2eaf05cb94c861eb1b8b44c3e3fdfbdef2812c94
lahoangquy <62043234+lahoangquy@users.noreply.github.com>
2026-05-13 21:15:03 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from agent import agent
+import uuid
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+@app.route('/')
+def home():
+    session['thread_id'] = str(uuid.uuid4())
+    if 'messages' not in session:
+        session['messages'] = []
+    print('home', session)
+    return render_template('chat.html', messages=session['messages'])
+
+@app.route('/send', methods=['POST'])
+def send():
+    user_message = request.form['message']
+    user_lat = request.form.get('latitude')
+    user_lon = request.form.get('longitude')
+    print(user_lat, user_lon)
+
+    if user_lat and user_lon:
+        session['user_location'] = {'lat': user_lat, 'lon': user_lon}
+
+    response = agent.invoke({"messages": [{'role': 'user', 'content':user_message}]},
+                                 {"configurable": {"thread_id":session['thread_id']}})
+    session['messages'].append({'type': 'human', 'content': user_message})
+    session['messages'].append({'type': 'ai', 'content': response['messages'][-1].content})
+    session.modified = True
+    print(session)
+    return redirect(url_for('home'))
+
+app.run(debug=True)
